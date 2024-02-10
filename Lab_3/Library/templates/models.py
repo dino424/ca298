b6e86509ad29049b2dce4ff0b5d22952fe0595fa
Diana <diana@gmail.com>
2024-02-10 14:58:45 +0000

    commiting to work for home

--- Lab_3/Library/templates/models.py
+++ Lab_3/Library/templates/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class Book(models.Model):
     id = models.IntegerField(primary_key = True)
@@ -8,3 +9,40 @@ class Book(models.Model):
     title = models.TextField()
     synopsis = models.TextField()
     category = models.TextField(default="fun")
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('wheres the email bro?')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.save(using=self._db)
+        return user
+    
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    email = models.EmailField('Email', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
