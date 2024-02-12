5c37f49c34ca7124892a4b7ab2eb9a45d4970604
Diana <diana@gmail.com>
2024-02-12 20:15:43 +0000

    commiting user signup

--- Project_1/pizza/pizzaapp/urls.py
+++ Project_1/pizza/pizzaapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .forms import *
 
 urlpatterns = [
-   path('', views.index, name="index")
+   path('', views.index, name="index"),
+   path('register/', views.UserSignupView.as_view(), name="register"),
+   path('login/',views.LoginView.as_view(template_name="login.html", authentication_form=UserLoginForm)),
+   path('logout/', views.logout_user, name="logout"),
+   path('register/', views.UserSignupView.as_view(), name="register")
 ]
--- Project_1/pizza/pizzaapp/views.py
+++ Project_1/pizza/pizzaapp/views.py
@@ -1,5 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView
+from .forms import *
 
 def index(request):
     return render(request, 'index.html')
+
+class UserSignupView(CreateView):
+    model = User
+    form_class = UserSignupForm
+    template_name = 'user_signup.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('/')
+
+
+class UserLoginView(LoginView):
+    template_name='login.html'
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
