cf2e807bf9e6d23019f6f4b03996447f3992a566
Diana <diana@gmail.com>
2024-02-12 21:13:00 +0000

    adding user signup

---
+++ Project_1/pizza/pizzaapp/forms.py
@@ -0,0 +1,23 @@
+# forms.py
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
+from django.forms import ModelForm, ModelChoiceField
+from .models import User
+from django.db import transaction
+
+class UserSignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_admin = False
+				user.email = self.cleaned_data['username']
+        user.save()
+        return user
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
