46c29e2d0e56c0c322f90249a081cd8faa2c3e28
Diana <diana@gmail.com>
2024-02-14 18:14:31 +0000

    all pizza display

--- Project/pizza/pizzaapp/urls.py
+++ Project/pizza/pizzaapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import * 
 
 urlpatterns = [
-   path('', views.index, name="index")
+   path('', views.index, name="index"),
+   path('pizzas', view_all_pizzas, name='all_pizzas'),
 ]
--- Project/pizza/pizzaapp/views.py
+++ Project/pizza/pizzaapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.views.generic import CreateView
+from .models import * 
+
 
 def index(request):
     return render(request, 'index.html')
+
+def view_all_pizzas(request):
+    all_pizzas = Pizza.objects.all()
+    return render(request, 'all_pizzas.html', {'pizzas':all_pizzas})
