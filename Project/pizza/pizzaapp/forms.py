e076431d1820ce9401e082bbffa07f260502339d
Diana Eze <diana.eze2@mail.dcu.ie>
2024-02-23 18:51:39 +0000

    final

--- Project/pizza/pizzaapp/forms.py
+++ Project/pizza/pizzaapp/forms.py
@@ -14,36 +14,34 @@ class PizzaForm(forms.ModelForm):
             'toppings': forms.CheckboxSelectMultiple(attrs={'class': 'checkbox-class'}),
         }
     def clean_name(self):
-        name = self.cleaned_data.get('name')
-        if not name:
+        name = self.cleaned_data['name']
+        if (len(name) == 0):
             raise forms.ValidationError("Your pizza nwants a name ૮ ˶ᵔ ᵕ ᵔ˶ ა ")
         return name
 
     def clean_sizes(self):
-        sizes = self.cleaned_data.get('sizes')
+        sizes = self.cleaned_data['sizes']
         if not sizes:
-            raise forms.ValidationError("Please select at least one size (˶ᵔ ᵕ ᵔ˶) ")
+            raise ValidationError("Please select at least one size (˶ᵔ ᵕ ᵔ˶) ")
         return sizes
 
     def clean_cheeses(self):
-        cheeses = self.cleaned_data.get('cheeses')
+        cheeses = self.cleaned_data['cheeses']
         if not cheeses:
-            raise forms.ValidationError("Please select at least one cheese option ૮꒰ ˶• ༝ •˶꒱ა ♡")
+            raise ValidationError("Please select at least one cheese option ૮꒰ ˶• ༝ •˶꒱ა ♡")
         return cheeses
 
     def clean_sauces(self):
-        sauces = self.cleaned_data.get('sauces')
+        sauces = self.cleaned_data['sauces']
         if not sauces:
-            raise forms.ValidationError("Please select at least one sauce option.˖⁺‧₊˚ ♡ ˚₊‧⁺˖")
+            raise ValidationError("Please select at least one sauce option.˖⁺‧₊˚ ♡ ˚₊‧⁺˖")
         return sauces
 
     def clean_crusts(self):
-        sauces = self.cleaned_data.get('crusts')
-        if not sauces:
-            raise forms.ValidationError("Every pizza needs a crust .˖⁺‧₊˚ ♡ ˚₊‧⁺˖")
-        return sauces
-
-
+        crusts = self.cleaned_data['crusts']
+        if not crusts:
+            raise ValidationError("Every Pizza needs a crust.˖⁺‧₊˚ ♡ ˚₊‧⁺˖")
+        return crusts
 
 
 class UserSignupForm(UserCreationForm):
@@ -66,22 +64,16 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['name','address', 'card_number', 'card_exp', 'card_cvv']
-
+        
     def clean_card_number(self):
         card_number = self.cleaned_data['card_number']
         if len(str(card_number)) != 16:
-            raise ValidationError('Card number must be 16 digits long.')
+            raise ValidationError('Card number must be 16 digits long.˖⁺‧₊˚ ♡ ˚₊‧⁺˖')
         return card_number
 
     def clean_card_cvv(self):
         card_cvv = self.cleaned_data['card_cvv']
         if len(str(card_cvv)) > 3 or len(str(card_cvv)) < 3:
-            raise ValidationError('CVV must be 3 digits long.')
+            raise ValidationError('CVV must be 3 digits long.  ૮꒰ ˶• ༝ •˶꒱ა ♡')
         return card_cvv
     
-    def clean(self):
-        cleaned_data = super().clean()
-        for field_name in self.Meta.fields:
-            if not cleaned_data.get(field_name):
-                raise ValidationError(f'Field {field_name} is missing')
-        return cleaned_data
